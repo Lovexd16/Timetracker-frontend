f327a041ae281c3698632c1eb1eeadb1a3039a2c
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-02 08:30:52 +0200

    Nu kan man lägga till nya tasks genom knapp
    Dock måste man refresha sidan för att listan ska uppdateras, måste lösas!
    fixes #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import AddTask from "./Components/AddTask";
 import ListTasks from "./Components/ListTasks";
 
 function App() {
   return (
     <>
-      <h1>Hello Tasks</h1>
+      <h1>Timetracker</h1>
+      <AddTask />
       <ListTasks />
     </>
   );
---
+++ src/Components/AddTask.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function AddTask() {
+  const [newTask, setNewTask] = useState<string>("");
+
+  const addNewTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch("https://jellyfish-app-4sahl.ondigitalocean.app/task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ taskName: newTask }),
+    }).then(() => setNewTask(""));
+  };
+
+  return (
+    <div>
+      <form onSubmit={addNewTask}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        ></input>
+        <button>Lägg till</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTask;
