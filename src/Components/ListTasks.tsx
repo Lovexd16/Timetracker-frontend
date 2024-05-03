a94226c00c19f468ffef3fac4737bce773a09c7e
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-03 13:13:06 +0200

    delat in i 2 olika komponenter

--- src/Components/ListTasks.tsx
+++ src/Components/ListTasks.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import TaskItem from "./TaskItem";
 
 interface Task {
   id: string;
   taskName: string;
 }
 
-function ListTasks() {
+function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
@@ -13,51 +14,6 @@ function ListTasks() {
       .then((res) => res.json())
       .then((data) => setTasks(data));
   }, []);
-
-  const TaskItem = ({ task }: { task: Task }) => {
-    const [time, setTime] = useState(0);
-    const [timerRunning, setTimerRunning] = useState(false);
-
-    useEffect(() => {
-      let interval;
-      if (timerRunning) {
-        interval = setInterval(() => {
-          setTime((prevTime) => prevTime + 10);
-        }, 10);
-      } else {
-        clearInterval(interval!);
-      }
-      return () => clearInterval(interval!);
-    }, [timerRunning]);
-
-    const saveTime = async () => {
-      setTimerRunning(false);
-
-      fetch(
-        `https://jellyfish-app-4sahl.ondigitalocean.app/task/${task.id}/time`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ time }),
-        }
-      );
-    };
-
-    return (
-      <div key={task.id}>
-        {task.taskName}
-        {timerRunning ? (
-          <button onClick={saveTime}>Stop timer</button>
-        ) : (
-          <button onClick={() => setTimerRunning(true)}>Start timer</button>
-        )}
-        <span>{"0" + Math.floor((time / 60000) % 60)}:</span>
-        <span>{"0" + Math.floor((time / 1000) % 60)}</span>
-      </div>
-    );
-  };
 
   return (
     <>
@@ -68,4 +24,4 @@ function ListTasks() {
   );
 }
 
-export default ListTasks;
+export default TaskList;
---
+++ src/Components/TaskItem.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+interface Task {
+  id: string;
+  taskName: string;
+}
+
+function TaskItem({ task }: { task: Task }) {
+  const [time, setTime] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (timerRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval!);
+    }
+    return () => clearInterval(interval!);
+  }, [timerRunning]);
+
+  const saveTime = async () => {
+    setTimerRunning(false);
+
+    fetch(
+      `https://jellyfish-app-4sahl.ondigitalocean.app/task/${task.id}/time`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ time }),
+      }
+    );
+  };
+
+  return (
+    <div key={task.id}>
+      {task.taskName}
+      {timerRunning ? (
+        <button onClick={saveTime}>Stop timer</button>
+      ) : (
+        <button onClick={() => setTimerRunning(true)}>Start timer</button>
+      )}
+      <span>{"0" + Math.floor((time / 60000) % 60)}:</span>
+      <span>{"0" + Math.floor((time / 1000) % 60)}</span>
+    </div>
+  );
+}
+
+export default TaskItem;
